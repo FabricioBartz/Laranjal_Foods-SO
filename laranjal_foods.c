ec7e2e12408228a9a4cabd6a35e354fc47ccf90f
Fabricio Fiss Bartz <fabriciofissbartz@gmail.com>
2026-02-17 21:56:05 -0300

    Update laranjal_foods.c

--- laranjal_foods.c
+++ laranjal_foods.c
@@ -7,12 +7,6 @@
 #define NUM_RESTAURANTES 5  // número de restaurantes
 #define NUM_ENTREGADORES 10 // número de entregadores
 
-// Estrutura para passar o ID do entregador para as threads
-typedef struct {
-    int id;
-} ContextoEntregador;
-
-
 // Protótipos das funções dos entregadores
 void* entregador_veterano(void* arg);
 void* entregador_novato(void* arg);
@@ -36,13 +30,13 @@ int main() {
 
     // Criando as Threads de Entregadores
     for (int i = 0; i < NUM_ENTREGADORES; i++) {
-        ContextoEntregador* ctx = malloc(sizeof(ContextoEntregador));
-        ctx->id = i;
+        int* id = malloc(sizeof(int)); // Aloca memória para o ID do entregador
+        *id = i;
         
         if (i % 2 == 0) 
-            pthread_create(&entregadores[i], NULL, entregador_veterano, ctx);
+            pthread_create(&entregadores[i], NULL, entregador_veterano, id);
         else 
-            pthread_create(&entregadores[i], NULL, entregador_novato, ctx);
+            pthread_create(&entregadores[i], NULL, entregador_novato, id);
     }
 
     // Aguardando as Threads terminarem (neste caso, elas rodarão indefinidamente)
@@ -56,8 +50,7 @@ int main() {
 // Função para os entregadores veteranos
 void* entregador_veterano(void* arg) {
     
-    ContextoEntregador* ctx = (ContextoEntregador*)arg;
-    int id_entregador = ctx->id;
+    int id_entregador = *(int*)arg;
 
     while (1) { 
         int restaurante = rand() % NUM_RESTAURANTES; // Sorteia a cada nova entrega
@@ -73,12 +66,13 @@ void* entregador_veterano(void* arg) {
             printf("[Veterano %d]: Entrega feita! Liberando Restaurante %d e voltando para a fila.\n", id_entregador, restaurante);
             pthread_mutex_unlock(&pedidos[restaurante]); // Libera o lanche
             pthread_mutex_unlock(&motos[restaurante]); // Libera a moto
-            
+            sleep(1); // Pequena pausa antes de pegar o próximo pedido
+
             
         } else {
             printf("[Veterano %d]: DEADLOCK! Devolvendo chave da moto do Restaurante %d para destravar.\n", id_entregador, restaurante);
             pthread_mutex_unlock(&motos[restaurante]); // Libera a moto para evitar deadlock
-           
+            usleep((rand() % 500 + 500) * 1000); // Pausa aleatória para evitar que o mesmo entregador tente o mesmo restaurante imediatamente
         }
     }
     free(ctx); 
@@ -88,8 +82,7 @@ void* entregador_veterano(void* arg) {
 // Função para os entregadores novatos
 void* entregador_novato(void* arg) {
     
-    ContextoEntregador* ctx = (ContextoEntregador*)arg;
-    int id_entregador = ctx->id;
+    int id_entregador = *(int*)arg;
 
     while (1) {  
         int restaurante = rand() % NUM_RESTAURANTES; 
@@ -105,12 +98,13 @@ void* entregador_novato(void* arg) {
             printf("[Novato %d]: Entrega feita! Liberando Restaurante %d e voltando para a fila.\n", id_entregador, restaurante);
             pthread_mutex_unlock(&motos[restaurante]); // Libera a moto
             pthread_mutex_unlock(&pedidos[restaurante]); // Libera o lanche
-            
+            sleep(1); // Pequena pausa antes de pegar o próximo pedido
+
            
         } else {
             printf("[Novato %d]: DEADLOCK! Devolvendo lanche do Restaurante %d para destravar.\n", id_entregador, restaurante);
             pthread_mutex_unlock(&pedidos[restaurante]); // Libera o lanche para evitar deadlock
-           
+            usleep((rand() % 500 + 500) * 1000); // Pausa aleatória para evitar que o mesmo entregador tente o mesmo restaurante imediatamente
         }
     }
     free(ctx);
